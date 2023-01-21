f5b662776cb4838c8bdc029e3c8d7467aa3d0b21
gbrohit <119616499+gbrohit@users.noreply.github.com>
2023-01-22 00:02:37 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -7,70 +7,8 @@ from utils_bot import *
 from Adarsh import StartTime
 
 
-START_TEXT = """ Your Telegram DC Is : `{}`  """
 
-
-@StreamBot.on_message(filters.regex("maintainers😎"))
-async def maintainers(b,m):
-    try:
-       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
-    except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="I am Coded By [Adarsh Goel](https://github.com/adarsh-goel)",
-                    
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("Developer💻", url=f"https://github.com/adarsh-goel")
-                            ]
-                        ]
-                    ),
-                    
-                    disable_web_page_preview=True)
-            
-         
-@StreamBot.on_message(filters.regex("follow❤️"))
-async def follow_user(b,m):
-    try:
-       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
-    except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="<B>HERE'S THE FOLLOW LINK</B>",
-                    
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("FOLLOW ME", url=f"https://GITHUB.COM/adarsh-goel")
-                            ]
-                        ]
-                    ),
-                    
-                    disable_web_page_preview=True)
-        
-
-@StreamBot.on_message(filters.regex("DC"))
-async def start(bot, update):
-    text = START_TEXT.format(update.from_user.dc_id)
-    await update.reply_text(
-        text=text,
-        disable_web_page_preview=True,
-        quote=True
-    )
-
-    
-    
-@StreamBot.on_message(filters.command("list"))
-async def list(l, m):
-    LIST_MSG = "Hi! {} Here is a list of all my commands \n \n 1 . `⚡️Start⚡️` \n 2. `📚Help📚` \n 3. `📡Ping📡` \n 4. `📊Status📊`"
-    await l.send_message(chat_id = m.chat.id,
-        text = LIST_MSG.format(m.from_user.mention(style="md"))
-        
-    )
-    
-    
-@StreamBot.on_message(filters.command("ping"))
+@StreamBot.on_message(filters.command('ping') & filters.private)
 async def ping(b, m):
     start_t = time.time()
     ag = await m.reply_text("....")
@@ -81,7 +19,7 @@ async def ping(b, m):
     
     
     
-@StreamBot.on_message(filters.command("status"))
+@StreamBot.on_message(filters.command('stats') & filters.private)
 async def stats(bot, update):
   currentTime = readable_time((time.time() - StartTime))
   total, used, free = shutil.disk_usage('.')
