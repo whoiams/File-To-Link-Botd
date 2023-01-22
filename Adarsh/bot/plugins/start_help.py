87bd2a10bc57cd533a9aa59dca3ea64fcf61d150
gbrohit <119616499+gbrohit@users.noreply.github.com>
2023-01-22 13:57:58 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -1,195 +1,3 @@
-#Aadhi000 
-from Adarsh.bot import StreamBot
-from Adarsh.vars import Var
-import logging
-logger = logging.getLogger(__name__)
-from Adarsh.bot.plugins.stream import MY_PASS
-from Adarsh.utils.human_readable import humanbytes
-from Adarsh.utils.database import Database
-from pyrogram import filters, enums
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import UserNotParticipant
-from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
-db = Database(Var.DATABASE_URL, Var.name)
-from pyrogram.types import ReplyKeyboardMarkup
-
-                      
-@StreamBot.on_message(filters.command('start') & filters.private)
-async def start(b, m):
-    if not await db.is_user_exist(m.from_user.id):
-        await db.add_user(m.from_user.id)
-        await b.send_message(
-            Var.BIN_CHANNEL,
-            f"**New User Joined**\n\n**New User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started Your Bot**"
-        )
-    usr_cmd = m.text.split("_")[-1]
-    if usr_cmd == "/start":
-        if Var.UPDATES_CHANNEL is not None:
-            try:
-                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-                if user.status == enums.ChatMemberStatus.BANNED:
-                    await b.send_message(
-                        chat_id=m.chat.id,
-                        text="**Sorry, You are Banned from Using this Bot**\n\n**Contact @The_Insomniacs_Club_Bot**",
-                        disable_web_page_preview=True
-                    )
-                    return
-            except UserNotParticipant:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="**Due to Overload, Only Channel Subscribers can Use the Bot**\n\n**Join Our Channel to Use This Bot**\n\n**After Joining Press Start Again**",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                            ]
-                        ]
-                    )
-                    
-                )
-                return
-            except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="**Something Went Wrong** <b> <a href='https://t.me/The_Insomniacs_Club_Bot'>CLICK HERE FOR SUPPORT </a></b>",
-                    
-                    disable_web_page_preview=True)
-                return
-        await m.reply_photo(
-            photo="https://te.legra.ph/file/8dfe7256883cbc0190478.jpg",
-            caption="**ʜᴇʟʟᴏ...⚡\n\nɪᴀᴍ ᴀ sɪᴍᴘʟᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ/ᴠɪᴅᴇᴏ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʟɪɴᴋ ᴀɴᴅ sᴛʀᴇᴀᴍ ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ.**\n\n**ᴜsᴇ /help ғᴏʀ ᴍᴏʀᴇ ᴅᴇᴛsɪʟs\n\nsᴇɴᴅ ᴍᴇ ᴀɴʏ ᴠɪᴅᴇᴏ / ғɪʟᴇ ᴛᴏ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀᴢ...**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("⚡ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 ⚡", url="https://t.me/kwicbotupdates"), InlineKeyboardButton("⚡ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ⚡", url="https://t.me/kwicbotupdates")],
-                    [InlineKeyboardButton("📺 24/7 𝙼𝙾𝚅𝙸𝙴𝚂 📺", url="https://t.me/MoviesNowV2"), InlineKeyboardButton("💎 𝙾𝚃𝚃 𝙼𝙾𝚅𝙸𝙴𝚂 💎", url="https://t.me/MoviesNowOTT2")],
-                    [InlineKeyboardButton("💌 𝙼𝙾𝚅𝙸𝙴 𝙱𝙾𝚃 💌", url="https://t.me/KWICVER2bot")]
-                ]
-            ),
-            
-        )
-    else:
-        if Var.UPDATES_CHANNEL is not None:
-            try:
-                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
-                if user.status == enums.ChatMemberStatus.BANNED:
-                    await b.send_message(
-                        chat_id=m.chat.id,
-                        text="**Sorry, You are Banned from Using this Bot**\n\n**Contact @The_Insomniacs_Club_Bot**",
-                        
-                        disable_web_page_preview=True
-                    )
-                    return
-            except UserNotParticipant:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="**Due to Overload, Only Channel Subscribers can Use the Bot**\n\n**Join Our Channel to Use This Bot**\n\n**After Joining Press Start Again**",
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                            ]                           
-                        ]
-                    )
-                    
-                )
-                return
-            except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="**Something Went Wrong** <b> <a href='https://t.me/The_Insomniacs_Club_Bot'>CLICK HERE FOR SUPPORT </a></b>",
-                    disable_web_page_preview=True)
-                return
-
-        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, ids=int(usr_cmd))
-
-        file_size = None
-        if get_msg.video:
-            file_size = f"{humanbytes(get_msg.video.file_size)}"
-        elif get_msg.document:
-            file_size = f"{humanbytes(get_msg.document.file_size)}"
-        elif get_msg.audio:
-            file_size = f"{humanbytes(get_msg.audio.file_size)}"
-
-        file_name = None
-        if get_msg.video:
-            file_name = f"{get_msg.video.file_name}"
-        elif get_msg.document:
-            file_name = f"{get_msg.document.file_name}"
-        elif get_msg.audio:
-            file_name = f"{get_msg.audio.file_name}"
-
-        stream_link = "https://{}/{}".format(Var.FQDN, get_msg.id) if Var.ON_HEROKU or Var.NO_PORT else \
-            "http://{}:{}/{}".format(Var.FQDN,
-                                     Var.PORT,
-                                     get_msg.id)
-
-        msg_text = "**ᴛᴏᴜʀ ʟɪɴᴋ ɪs ɢᴇɴᴇʀᴀᴛᴇᴅ...⚡\n\n📧 ғɪʟᴇ ɴᴀᴍᴇ :-\n{}\n {}\n\n💌 ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :- {}\n\n♻️ ᴛʜɪs ʟɪɴᴋ ɪs ᴘᴇʀᴍᴀɴᴇɴᴛ ᴀɴᴅ ᴡᴏɴ'ᴛ ɢᴇᴛ ᴇxᴘɪʀᴇᴅ ♻️\n\n@MoviesNowV2**"
-        await m.reply_photo(
-            photo="https://te.legra.ph/file/8dfe7256883cbc0190478.jpg",
-            caption=msg_text.format(file_name, file_size, stream_link),
-            
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⚡ ᴅᴏᴡɴʟᴏᴀᴅ ɴᴏᴡ ⚡", url=stream_link)]])
-        )
-
-
-@StreamBot.on_message(filters.command('help') & filters.private)
-async def help_handler(bot, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id)
-        await bot.send_message(
-            Var.BIN_CHANNEL,
-            f"**New User Joined**\n\n**New User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started Your Bot**"
-        )
-    if Var.UPDATES_CHANNEL is not None:
-        try:
-            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
-            if user.status == enums.ChatMemberStatus.BANNED:
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text="**Sorry, You are Banned from Using this Bot**\n\n**Contact @The_Insomniacs_Club_Bot**",
-                    
-                    disable_web_page_preview=True
-                )
-                return
-        except UserNotParticipant:
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text="**Due to Overload, Only Channel Subscribers can Use the Bot**\n\n**Join Our Channel to Use This Bot**\n\n**After Joining Press Start Again**",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                        ]
-                    ]
-                )
-            )
-            return
-        except Exception:
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text="**Something Went Wrong** <b> <a href='https://t.me/The_Insomniacs_Club_Bot'>CLICK HERE FOR SUPPORT </a></b>",
-                
-                disable_web_page_preview=True)
-            return
-    await message.reply_photo(
-            photo="https://te.legra.ph/file/8dfe7256883cbc0190478.jpg",
-            caption="**┣⪼ Send Me Any File or Video I will Give you Streamable and Downloadable link\n\n┣⪼ ᴛʜɪs ʟɪɴᴋ ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴏʀ ᴛᴏ sᴛʀᴇᴀᴍ ᴜsɪɴɢ ᴇxᴛᴇʀɴᴀʟ ᴠɪᴅᴇᴏ ᴘʟᴀʏᴇʀs ᴛʜʀᴏᴜɢʜ ᴍʏ sᴇʀᴠᴇʀs.\n\n┣⪼ ғᴏʀ sᴛʀᴇᴀᴍɪɴɢ ᴊᴜsᴛ ᴄᴏᴘʏ ᴛʜᴇ ʟɪɴᴋ ᴀɴᴅ ᴘᴀsᴛᴇ ɪᴛ ɪɴ ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴘʟᴀʏᴇʀ ᴛᴏ sᴛᴀʀᴛ sᴛʀᴇᴀᴍɪɴɢ.\n\n┣⪼ ᴛʜɪs ʙᴏᴛ ɪs ᴀʟsᴏ sᴜᴘᴘᴏʀᴛ ɪɴ ᴄʜᴀɴɴᴇʟ. ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀs ᴀᴅᴍɪɴ ᴛᴏ ɢᴇᴛ ʀᴇᴀʟᴛɪᴍᴇ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ғᴏʀ ᴇᴠᴇʀʏ ғɪʟᴇs/ᴠɪᴅᴇᴏs ᴘᴏsᴛ../\n\n sᴇɴᴅ 𝟸 ғɪʟᴇs ᴘᴇʀ 𝟻 ᴍɪɴɪᴛᴜᴇs(sᴘᴀᴍ = ʙᴀɴ)\n\n 𝗗𝗢𝗡𝗧 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 𝗣𝗢𝗥𝗡🔞\n\n\n┣⪼ ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ :- /about\n\n\nᴘʟᴇᴀsᴇ sʜᴀʀᴇ ᴀɴᴅ sᴜʙsᴄʀɪʙᴇ**", 
-  
-        
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("⚡ 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 ⚡", url="https://t.me/kwicbotupdates"), InlineKeyboardButton("⚡ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 ⚡", url="https://t.me/kwicbotupdates")],
-                [InlineKeyboardButton("📺 24/7 𝙼𝙾𝚅𝙸𝙴𝚂 📺", url="https://t.me/MoviesNowV2"), InlineKeyboardButton("💎𝙾𝚃𝚃 𝙼𝙾𝚅𝙸𝙴𝚂💎", url="https://t.me/MoviesNowOTT2")],
-                [InlineKeyboardButton("💌 𝙼𝙾𝚅𝙸𝙴 𝙱𝙾𝚃 💌", url="https://t.me/KWICVER2bot")]
-            ]
-        )
-    )
-
-
-
-
-
-
 # (c) adarsh-goel 
 from Adarsh.bot import StreamBot
 from Adarsh.vars import Var
@@ -204,9 +12,9 @@ from pyrogram.errors import UserNotParticipant
 from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
-           
-            
-@StreamBot.on_message(filters.command('start') & filters.private)
+
+                     
+@StreamBot.on_message((filters.command("start") & filters.private )
 async def start(b, m):
     if not await db.is_user_exist(m.from_user.id):
         await db.add_user(m.from_user.id)
@@ -227,12 +35,12 @@ async def start(b, m):
         except UserNotParticipant:
              await StreamBot.send_photo(
                 chat_id=m.chat.id,
-                photo="https://te.legra.ph/file/128832fdf95ad28ec4f14.jpg",
-                caption="**Join Our Channel to Use This Bot**",
+                photo="https://telegra.ph/file/9d94fc0af81234943e1a9.jpg",
+                caption="**Due to Overload, Only Channel Subscribers can Use the Bot**\n\n**Join Our Channel to Use This Bot**\n\n**After Joining Press Start Again**",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("Join Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
                         ]
                     ]
                 ),
@@ -249,10 +57,11 @@ async def start(b, m):
     await StreamBot.send_video(
         chat_id=m.chat.id,
         video ="https://te.legra.ph/file/788e1f924f7d8ece7b507.mp4",
-        caption =f'🫡 **Hey {m.from_user.mention(style="md")}**\n\n**I am Telegram File to Link Generator Bot**.\n\n**Send Me Any File or Video and Get a Direct Download Link and Streamable Link**')
+        caption =f'🫡 **Hey {m.from_user.mention(style="md")}**\n\n**I am Telegram File to Link Generator Bot**.\n\n**Send Me Any File or Video and Get a Direct Download Link and Streamable Link**',
+        )
 
 
-@StreamBot.on_message(filters.command('help') & filters.private)
+@StreamBot.on_message((filters.command("help") & filters.private )
 async def help_handler(bot, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id)
@@ -266,7 +75,7 @@ async def help_handler(bot, message):
             if user.status == "kicked":
                 await bot.send_message(
                     chat_id=message.chat.id,
-                    text="**Sorry, You are Banned from Using this Bot.\n\nContact @The_Insomniacs_Club_Bot",
+                    text="**Sorry, You are Banned from Using this Bot**\n\n**Contact @The_Insomniacs_Club_Bot**",
                     
                     disable_web_page_preview=True
                 )
@@ -274,12 +83,12 @@ async def help_handler(bot, message):
         except UserNotParticipant:
             await StreamBot.send_photo(
                 chat_id=message.chat.id,
-                photo="https://te.legra.ph/file/128832fdf95ad28ec4f14.jpg",
-                Caption="**Join Our Channel to Use This Bot**\n\n**Due to Overload, Only Channel Subscribers can Use the Bot**",
+                photo="https://telegra.ph/file/ca10e459bc6f48a4ad0f7.jpg",
+                Caption="**Due to Overload, Only Channel Subscribers can Use the Bot**\n\n**Join Our Channel to Use This Bot**\n\n**After Joining Press Start Again**",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("Join Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            InlineKeyboardButton("Jᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
                         ]
                     ]
                 ),
@@ -289,68 +98,17 @@ async def help_handler(bot, message):
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="Something Went Wrong.Contact Me [TIC Support](https://t.me/The_Insomniacs_Club_Bot).",
+                text="**Something Went Wrong** <b> <a href='https://t.me/The_Insomniacs_Club_Bot'>CLICK HERE FOR SUPPORT </a></b>",
                 disable_web_page_preview=True)
             return
     await message.reply_text(
-        text="""<b>Send Me Any File or Video I will Give you Streamable Link and Downloadable link.</b>\n
-<b>I also Support Channels, Add Me to your Channel and Send Any Media Files and See Miracle✨</b>""",
+        text="""**┣⪼ Send Me Any File or Video I will Give you Streamable and Downloadable link**\n\n**┣⪼ This Bot also Supports in Channels. Add Me to your Channel as Admin and Get Real Time Links**\n\n**┣⪼ Send 2 Files Per 5 Minutes to Avoid Spam (Spam = Ban)**\n\n**┣⪼ Try to Avoid Porn Content 🔞**""",
         
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Support", url="https://t.me/The_Insomniacs_Club_Bot")],
-            ]
-        )
-    )
-
-@StreamBot.on_message(filters.command('about') & filters.private)
-async def about_handler(bot, message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id)
-        await bot.send_message(
-            Var.BIN_CHANNEL,
-            f"**New User Joined**\n\n**New User [{message.from_user.first_name}](tg://user?id={message.from_user.id}) Started Your Bot**"
-        )
-    if Var.UPDATES_CHANNEL is not None:
-        try:
-            user = await bot.get_chat_member(Var.UPDATES_CHANNEL, message.chat.id)
-            if user.status == enums.ChatMemberStatus.BANNED:
-                await bot.send_message(
-                    chat_id=message.chat.id,
-                    text="**You are Banned**",
-                    
-                    disable_web_page_preview=True
-                )
-                return
-        except UserNotParticipant:
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text="**Join Our Channel to Use This Bot**\n\n**Due to Overload, Only Channel Subscribers can Use the Bot**",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Join Our Channel", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
-                        ]
-                    ]
-                )
-            )
-            return
-    await message.reply_photo(
-            photo="https://te.legra.ph/file/128832fdf95ad28ec4f14.jpg",
-            caption="""<b>Extra Info</b>
-<b>╭━━━━━━━〔TIC File To Link Bot〕</b>
-┣⪼<b>Developer : <a href='https://t.me/DANGER1753'>DANGER</a></b>
-┣⪼<b>Bot Name : <a href='https://t.me/The_Insomniacs_Club_5_Bot'>TIC File To Link Bot</a></b>
-┣⪼<b>sᴏᴜʀᴄᴇ-ᴄᴏᴅᴇ : <a href='https://t.me/The_Insomniacs_Club_Bot'>TIC File To Link Bot</a></b>
-┣⪼<b>ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ</b>
-┣⪼<b>ʟᴀɴɢᴜᴀɢᴇ: ᴘʏᴛʜᴏɴ 3</b>
-<b>╰━━━━━━━ (Thank You) </b>""",
-  
-        
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("Support", url="https://t.me/The_Insomniacs_Club_Bot")]
+                [InlineKeyboardButton("⚡ Support ⚡", url="https://t.me/The_Insomniacs_Club")],
+                [InlineKeyboardButton("💌 Developer 💌", url="https://t.me/DANGER1753")]
             ]
         )
     )
