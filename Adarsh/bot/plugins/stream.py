eaf5651646c810cd039b3814b8593623ac9c6768
kiran175333 <100829571+kiran175333@users.noreply.github.com>
2023-03-14 15:57:01 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -99,7 +99,7 @@ async def private_receive_handler(c: Client, m: Message):
         stream_link = f"{Var.URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
         online_link = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
        
-        msg_text ="""<u>𝐘𝐎𝐔𝐑 𝐋𝐈𝐍𝐊𝐒 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 :</u>\n\n<b>📂 ғɪʟᴇ ɴᴀᴍᴇ :</b> {}\n\n<b>📦 ғɪʟᴇ sɪᴢᴇ :</b> {}\n\n<b>📥 ᴅᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b>🖥 ᴡᴀᴛᴄʜ :</b> <i>{}</i>\n\n<b>🚸 ʟɪɴᴋs ᴅᴏɴ'ᴛ ᴇxᴘɪʀᴇ & ᴀʟʟ ᴄᴏɴᴛᴇɴᴛs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ [🔞✅] </b>\n\n<b>🚸 ᴏɴʟɪɴᴇ ᴡᴀᴛᴄʜɪɴɢ ʟɪɴᴋs ᴀʀᴇ sʟᴏᴡ ɪɴ ᴘᴄ / ʟᴀᴘᴛᴏᴘs </b>"""
+        msg_text ="""<u>𝐘𝐎𝐔𝐑 𝐋𝐈𝐍𝐊𝐒 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 :</u>\n\n<b>📂 ғɪʟᴇ ɴᴀᴍᴇ :</b> {}\n\n<b>📦 ғɪʟᴇ sɪᴢᴇ :</b> {}\n\n<b>📥 ᴅᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b>🖥 sᴛʀᴇᴀᴍ :</b> <i>{}</i>\n\n<b>🚸 ʟɪɴᴋs ᴅᴏɴ'ᴛ ᴇxᴘɪʀᴇ & ᴀʟʟ ᴄᴏɴᴛᴇɴᴛs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ [🔞✅] </b>\n\n<b>🚸 sᴛʀᴇᴀᴍ ʟɪɴᴋs ᴀʀᴇ sʟᴏᴡ ɪɴ ᴘᴄ / ʟᴀᴘᴛᴏᴘs </b>"""
 
         await log_msg.reply_text(text=f"**Requested By :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User ID :** `{m.from_user.id}`", disable_web_page_preview=True,  quote=True)
         await m.reply_text(
